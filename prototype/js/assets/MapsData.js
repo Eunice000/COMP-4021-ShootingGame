ef9ae9dc30a41bb2bea89aea2d91459b750b03c1
Porter <62560962+HPChung752@users.noreply.github.com>
2025-11-28 17:56:47 +0800

    fix 404 error on game assets

--- prototype/js/assets/MapsData.js
+++ prototype/js/assets/MapsData.js
@@ -3,21 +3,21 @@
     const maps = [
         {
             id: "testing",
-            background: "assets/game/backgrounds/map_background_testing.png",
+            background: "prototype/assets/game/backgrounds/map_background_testing.png",
             platforms: [{x: 192, y: 950, w: 1536, h: 8}],
             playerSpawns: [{x: 344, y: 700}, {x: 1496, y: 700}],
             powerUpSpawns: [{x: 536, y: 850}, {x: 920, y: 850}, {x: 1304, y: 850}]
         },
         {
             id: "testing2",
-            background: "assets/game/backgrounds/map_background_testing2.png",
+            background: "prototype/assets/game/backgrounds/map_background_testing2.png",
             platforms: [{x: 192, y: 950, w: 1536, h: 8}, {x: 192, y: 600, w: 1536, h: 8}],
             playerSpawns: [{x: 344, y: 700}, {x: 1496, y: 700}],
             powerUpSpawns: [{x: 536, y: 850}, {x: 920, y: 850}, {x: 1304, y: 850}]
         },
         {
             id: "map1",
-            background: "assets/game/backgrounds/map_background_map1.png",
+            background: "prototype/assets/game/backgrounds/map_background_map1.png",
             platforms: [
                 // level 1 (Top)
                 {x: 100, y: 300, w: 700, h: 8},
